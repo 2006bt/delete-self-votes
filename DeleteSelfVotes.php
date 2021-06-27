4f5d570ccf0af13d36ba622bcbd8758ac34fe786
2006bt <m2006bt@outlook.com>
2021-06-27 21:43:01 +0800

    1

--- DeleteSelfVotes.php
+++ DeleteSelfVotes.php
@@ -16,25 +16,24 @@ echo "Connected to MySQL";
 // Query origin data
 $post_votes = $conn->query("SELECT id, post_id, user_id FROM post_votes");
 $posts = $conn->query("SELECT id, user_id FROM posts");
-$data_post=array(array());
+$data_posts=array(array());
 while($row = $posts->fetch_assoc()){
-    $data_post[]=$row;
+    $data_posts[]=$row;
 }
 
 // Process data
 $ids_to_delete = array();
-while($data_post_votes = $post_votes->fetch_assoc()){
-    $uid = $data_post_votes["user_id"];
-    var_dump(array_column($data_posts, 'user_id'));
-    /*if($uid == array_search($uid, array_column($data_posts, 'user_id'))){
-        $ids_to_delete[] =  $data_post_votes["id"];
-    }*/
+while($row = $post_votes->fetch_assoc()){
+    $uid = $row["user_id"];
+    if($uid == array_search($uid, array_column($data_posts, 'user_id'))){
+        $ids_to_delete[] =  $row["id"];
+    }
 }
 
 // Show result
 while($row = $post_votes->fetch_assoc()){
     if(in_array($row["id"], $ids_to_delete)){
-        echo "ID" . $row["id"] . "user_id" . $row["user_id"];
+        echo "ID" . $row["id"] . "user_id" . $row["user_id"] . "\n";
     }
 }
 echo "We will delete these.";
